6ca994a77d4c13fd180238683fdb23bd2f3f2871
kahlamari <email@vincent-kahl.net>
2023-12-18 15:38:37 +0100

    Add searchByOrderStatus

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
@@ -20,5 +21,9 @@ public class ShopService {
         Order newOrder = new Order(UUID.randomUUID().toString(), products, Order.OrderStatus.PROCESSING);
 
         return orderRepo.addOrder(newOrder);
+    }
+
+    public List<Order> searchByOrderStatus(Order.OrderStatus orderStatus) {
+        return orderRepo.getOrders().stream().filter(o -> o.orderStatus().equals(orderStatus)).collect(Collectors.toList());
     }
 }
